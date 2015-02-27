e3d100605f1af26a0385a77db3c4be51e3ef1620
christian <etudiant@192.168.10.34>
2015-02-27 21:28:52 +0100

    test2

--- src/codeac/Test.java
+++ src/codeac/Test.java
@@ -1,10 +1,13 @@
 package codeac;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour tout le monde");
+	
 	}
 
 }
---
+++ src/codeac/test2.java
@@ -0,0 +1,11 @@
+package codeac;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		System.out.println("salut");
+		// TODO Auto-generated method stub
+
+	}
+
+}
